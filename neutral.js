1064c2fbd8b1a9df9bcc828683659210cd6614ab
Yun-Fang Juan <yunfangjuan@Yun-Fangs-MacBook-Pro-2.local>
2021-02-18 22:22:51 -0800

    market neutral

--- neutral.js
+++ neutral.js
@@ -3,6 +3,8 @@ const { Contract, providers, Wallet, utils } = require("ethers")
 const AmmReaderArtifact = require("@perp/contract/build/contracts/AmmReader.json")
 const InsuranceFundArtifact = require("@perp/contract/build/contracts/InsuranceFund.json")
 const ERC20Contract = require('erc20-contract-js');
+const Uniswap = require('@uniswap/sdk')
+//importing SDK
 
 
 //command line input https://nodejs.org/en/knowledge/command-line/how-to-prompt-for-command-line-input/
@@ -30,7 +32,32 @@ async function setupERC20Token(tokenAddress) {
 
 }
 
-async function checkUNIPrice(contractPrice) {
+async function checkETHPriceOnUniswap() {
+  const chainId = Uniswap.ChainId.MAINNET
+  const decimals = 18
+  const USDC = new Uniswap.Token(chainId, '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', 6)
+  const WETH = new Uniswap.Token(chainId, '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', 18)
+  const USDC_WETH_PAIR_ADDR = Uniswap.Pair.getAddress(USDC, WETH)
+  console.log('USDC-ETH pair address:', USDC_WETH_PAIR_ADDR)
+  const pair = await Uniswap.Fetcher.fetchPairData(USDC, WETH)
+  const route = new Uniswap.Route([pair], WETH)
+  const usdc_reserve = pair.reserveOf(USDC)
+  const eth_reserve = pair.reserveOf(WETH)
+  const usdc_total = usdc_reserve.numerator/usdc_reserve.denominator
+  const eth_total = eth_reserve.numerator/eth_reserve.denominator
+
+  const swap_price = usdc_total/eth_total
+  console.log(swap_price);
+
+  const route_price = route.midPrice.raw.toSignificant(12) * (10**12)
+
+  console.log('ETH/USDC:', route_price) // 201.306
+  //console.log('USDC/ETH', route.midPrice.invert().raw.toSignificant(6)) // 0.00496756
+  return { USDC_WETH_PAIR_ADDR,  route_price }
+
+}
+
+async function getETHBalance {
 
 }
 
@@ -76,12 +103,23 @@ async function main() {
   const symbol =  await erc20.symbol()
   perp_balance /= 10**decimals;
   console.log(`${symbol} balance for ${wallet.address}: ${perp_balance}`);
-
-
-  
-  
   const ammInfos = ammProps.map(prop => getAmmInfo(prop))
   console.log(ammInfos)
+  checkETHPriceOnUniswap()
+}
+
+async function runMarketNeutral() {
+  var { insuranceFund, ammReader, wallet } = await setupEnv()
+  const ETH_STOP_PRICE = 1200
+  // if eht price is less than the ETH STOP price, liquidate everything
+
+
+  // check wallet balance for LP-ETH-USDC token on uniswap
+
+  // getting # ETH that should be shorted
+
+  // check L2 ETH balance. If difference is > 5%, add / reduce ETH contracts
+
 }
 
 main()
