058f3913b2bf2d6bd9d38fc096a16e6e21e9c197
Yun-Fang Juan <yunfangjuan@Yun-Fangs-MacBook-Pro-2.local>
2021-03-10 14:18:30 -0800

    getting all the wallets interacting with furu

--- furucombo.js
+++ furucombo.js
@@ -4,6 +4,7 @@ const AmmReaderArtifact = require("@perp/contract/build/contracts/AmmReader.json
 const InsuranceFundArtifact = require("@perp/contract/build/contracts/InsuranceFund.json")
 const ERC20Contract = require('erc20-contract-js');
 const Uniswap = require('@uniswap/sdk')
+var fs = require('fs').promises;
 import axios from 'axios'
 
 var Web3 = require('web3');
@@ -44,11 +45,27 @@ async function getApprovals(account, contract) {
 
 }
 
-async function getWalletsForContract(contract, start_block, end_block) {
+
+async function getWalletsForContract(contract, start_block, end_block, output_file) {
   const batchSize = 100;
-
-
-
+  var pageNum = 1;
+  var count = batchSize;
+  var totalCount = 0;
+  await fs.writeFile(output_file, '', {flag: "w"});
+  try {
+    while ((count+0.1) >= batchSize) {
+      const transactions = await api.account.txlist(contract, start_block, end_block, pageNum, batchSize, 'asc')
+      count = transactions.result.length;
+      totalCount += count;
+      console.log("Start Block: ", start_block, "Total transactions: ", totalCount);
+      for (var j = 0; j < count; j++) {
+        await fs.writeFile(output_file, transactions.result[j].from + "\n", {flag: "a"});
+      }
+      pageNum++;
+    }
+  } catch (error) {
+     console.log(error);
+  }
 }
 
 async function getTokenBalanceforWallet(wallet, token, block) {
@@ -65,8 +82,15 @@ async function main() {
   const block = 11900892
   const perp_token =  '0xbC396689893D065F41bc2C6EcbeE5e0085233447';
   
-  const perp_balance = await getTokenBalanceforWallet(wallet, perp_token, block)
-  console.log(perp_balance);
+  //const perp_balance = await getTokenBalanceforWallet(wallet, perp_token, block)
+  //console.log(perp_balance);
+
+  const furu_contract = '0x17e8ca1b4798b97602895f63206afcd1fc90ca5f'
+  const last_furu_block = 11940504 
+  const first_furu_block = 11618386
+  const middle_block = parseInt((11940504 + 11618386)/2)
+  getWalletsForContract(furu_contract, first_furu_block, middle_block, 'furu_wallets.1.txt') 
+  getWalletsForContract(furu_contract, middle_block +1, last_furu_block, 'furu_wallets.2.txt') 
 
 }
 
