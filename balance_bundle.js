3b73c7fa27ed2cf5c04b43b8cc14f145dadf14e4
Evelyn Yang <changminyang@MacBook-Pro-72.local>
2021-04-01 19:10:00 -0700

    fix title

--- balance_bundle.js
+++ balance_bundle.js
@@ -247,6 +247,14 @@ async function addRowToHTML(token){
   $(function () {
     $('[data-toggle="popover"]').popover({html: true})
   })
+  $('body').on('click', function (e) {
+      $('[data-toggle=popover]').each(function () {
+          // hide any open popovers when the anywhere else in the body is clicked
+          if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
+              $(this).popover('hide');
+          }
+      });
+  });
 }
 
 async function getBalanceList(account, block){
--- balance_functions.js
+++ balance_functions.js
@@ -127,6 +127,15 @@ async function addRowToHTML(token){
   $(function () {
     $('[data-toggle="popover"]').popover({html: true})
   })
+  //http://bootply.com/99372
+  $('body').on('click', function (e) {
+      $('[data-toggle=popover]').each(function () {
+          // hide any open popovers when the anywhere else in the body is clicked
+          if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
+              $(this).popover('hide');
+          }
+      });
+  });
 }
 
 async function getBalanceList(account, block){
